4a58d407727a5dc67b6fc9e8af0dbabbffdf6cb1
Margarita Mareeva <beffinda@gmail.com>
2023-02-27 20:09:32 +0300

    configuring requests and responses

---
+++ http.php
@@ -0,0 +1,16 @@
+<?php
+
+use Beffi\advancephp\Blog\Http\Request;
+use Beffi\advancephp\Blog\Http\SuccessfulResponse;
+
+require_once __DIR__ . '/vendor/autoload.php';
+$request = new Request($_GET, $_SERVER);
+$parameter = $request->query('some_parameter');
+$header = $request->header('Some-Header');
+$path = $request->path();
+// Создаём объект ответа
+$response = new SuccessfulResponse([
+    'message' => 'Hello from PHP',
+]);
+// Отправляем ответ
+$response->send();
---
+++ src/Blog/Exceptions/HttpException.php
@@ -0,0 +1,7 @@
+<?php
+namespace Beffi\advancephp\Blog\Exceptions;
+
+class HttpException extends AppException
+{
+
+}
